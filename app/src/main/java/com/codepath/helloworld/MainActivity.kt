dd93d96c04731a303c8caa4d14b8116ccabc3fde
John Doe <nnadibright33@gmail.com>
2022-12-28 12:31:16 -0600

    Initial commit

---
+++ app/src/main/java/com/codepath/helloworld/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.codepath.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.Hellobutton)
+        val button = findViewById<Button>(R.id.Hellobutton)
+        button.setOnClickListener {
+            Log.v("Hello World","Button CLicked")
+            Toast.makeText(this,"Hello to you too!",Toast.LENGTH_SHORT).show()
+
+        }
+    }
+}
